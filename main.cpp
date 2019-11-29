14aebf3afdb3403a711771dd0c910bf2963c2e74
Rustam <rustam.guseynov@refinitiv.com>
2019-11-29 08:37:31 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -8,29 +8,14 @@ using namespace std;
 
 int main() {
   uint32_t N;
-  int64_t sum = 0;
-  int64_t average = 0;
-  cin >> N;
-  vector<int32_t> numbers(N, 0);
-  for (auto &item : numbers) {
-    cin >> item;
-    sum += item;
+  uint64_t R;
+  cin >> N >> R;
+
+  uint64_t mass = 0;
+  for (uint32_t i = 0; i < N; i++) {
+    uint64_t W, H, D;
+    cin >> W >> H >> D;
+    mass += R * W * H * D;
   }
-  average = sum / N;
-  // cout << "S = " << sum << "; A = " << average << endl;
-  auto K = 0u;
-  uint32_t pos = 0;
-  vector<int32_t> greaters;
-  for (const auto &item : numbers) {
-    if (item > average) {
-      K++;
-      greaters.push_back(pos);
-    }
-    pos++;
-  }
-  cout << K << endl;
-  for (const auto &item : greaters) {
-    cout << item << " ";
-  }
-  cout << endl;
+  cout << mass << endl;
 }
