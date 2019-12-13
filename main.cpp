cc6360c4eec2e1d314e61472fb984d714b7dd72a
Rustam <rustam.guseynov@refinitiv.com>
2019-12-13 15:07:01 +0300

    In progress

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+#pragma region TEST
 template <class T> ostream &operator<<(ostream &os, const vector<T> &s) {
   os << "{";
   bool first = true;
@@ -88,50 +89,165 @@ public:
 private:
   int fail_count = 0;
 };
+#pragma endregion
 
-int GetDistinctRealRootCount(double a, double b, double c) {
-  if (a == 0.0 && b == 0.0 && c != 0.0)
-    return 0;
+#define LOCAL
 
-  if (a == 0.0 && b != 0.0)
-    return 1;
-
-  auto D = b * b - 4 * a * c;
-  if (D > 0) {
-    return 2;
-  } else if (D < 0) {
-    return 0;
-  } else {
-    return 1;
+#ifdef LOCAL
+class Rational {
+public:
+  Rational() {
+    num = 0;
+    denom = 1;
   }
+
+  Rational(int numerator, int denominator) {
+
+    if (denominator == 0) {
+      throw invalid_argument("denominator is 0");
+    }
+
+    if (numerator == 0) {
+      num = 0;
+      denom = 1;
+    } else {
+      auto divisor = gcd(abs(numerator), abs(denominator));
+      num = numerator / divisor;
+      denom = denominator / divisor;
+    }
+
+    if (denom < 0) {
+      num = -num;
+      denom = -denom;
+    }
+  }
+
+  int Numerator() const { return num; }
+
+  int Denominator() const { return denom; }
+
+private:
+  int num, denom;
+
+  int gcd(int n1, int n2) {
+    auto smallest = min(n1, n2);
+    auto largest = max(n1, n2);
+
+    if (smallest == 0 || smallest == 1) {
+      return 1;
+    }
+
+    while (smallest != 0) {
+      auto m = largest % smallest;
+      largest = smallest;
+      smallest = m;
+    }
+    return largest;
+  }
+};
+
+Rational operator+(const Rational &a, const Rational &b) {
+  return Rational(a.Numerator() * b.Denominator() +
+                      b.Numerator() * a.Denominator(),
+                  a.Denominator() * b.Denominator());
 }
 
-void TestZeroAB() {
-  AssertEqual(GetDistinctRealRootCount(0, 0, 1), 0);
-  AssertEqual(GetDistinctRealRootCount(0, 0, 10), 0);
-  AssertEqual(GetDistinctRealRootCount(0, 0, 100), 0);
-  AssertEqual(GetDistinctRealRootCount(0, 0, -1), 0);
-  AssertEqual(GetDistinctRealRootCount(0, 0, -1000), 0);
+Rational operator-(const Rational &a, const Rational &b) {
+  return Rational(a.Numerator() * b.Denominator() -
+                      b.Numerator() * a.Denominator(),
+                  a.Denominator() * b.Denominator());
 }
 
-void TestZeroANonZeroB() {
-  AssertEqual(GetDistinctRealRootCount(0, 1, 1), 1);
-  AssertEqual(GetDistinctRealRootCount(0, 1, 10), 1);
-  AssertEqual(GetDistinctRealRootCount(0, 10000, 100), 1);
-  AssertEqual(GetDistinctRealRootCount(0, -100, -1), 1);
-  AssertEqual(GetDistinctRealRootCount(0, 0.1, -1000), 1);
+Rational operator*(const Rational &a, const Rational &b) {
+  return Rational(a.Numerator() * b.Numerator(),
+                  a.Denominator() * b.Denominator());
 }
 
-void TestOthers() {
-  AssertEqual(GetDistinctRealRootCount(1, 3, 1), 2);
-  AssertEqual(GetDistinctRealRootCount(1, 2, 1), 1);
-  AssertEqual(GetDistinctRealRootCount(1, 1, 1), 0);
+Rational operator/(const Rational &a, const Rational &b) {
+  if (b.Numerator() == 0) {
+    throw domain_error("Division by zero");
+  }
+  return Rational(a.Numerator() * b.Denominator(),
+                  a.Denominator() * b.Numerator());
+}
+
+bool operator==(const Rational &a, const Rational &b) {
+  return a.Numerator() == b.Numerator() && a.Denominator() == b.Denominator();
+}
+
+bool operator!=(const Rational &a, const Rational &b) { return !(a == b); }
+
+bool operator<(const Rational &a, const Rational &b) {
+  return a.Numerator() * b.Denominator() < a.Denominator() * b.Numerator();
+}
+
+ostream &operator<<(ostream &stream, const Rational &a) {
+  stream << a.Numerator() << "/" << a.Denominator();
+  return stream;
+}
+
+#endif
+
+void TestDefaultConstructor() {
+  Rational a;
+  AssertEqual(a.Numerator(), 0, "0");
+  AssertEqual(a.Denominator(), 1, "1");
+}
+
+void TestGcd() {
+  Rational a(4, 6);
+  AssertEqual(a.Numerator(), 2, "a");
+  AssertEqual(a.Denominator(), 3, "b");
+  AssertEqual(a, Rational(2, 3), "c");
+
+  Rational b(1, 3);
+  AssertEqual(b.Numerator(), 1, "d");
+  AssertEqual(b.Denominator(), 3, "e");
+
+  AssertEqual(Rational(4, 6), Rational(2, 3), "f");
+  AssertEqual(Rational(17, 6), Rational(17, 6), "g");
+  AssertEqual(Rational(22, 22), Rational(1, 1), "h");
+}
+
+void TestNegativeRational() {
+  AssertEqual(Rational(-1, 22), Rational(-1, 22));
+  AssertEqual(Rational(1, -100), Rational(-1, 100));
+  AssertEqual(Rational(-1, 1), Rational(-1, 1));
+  AssertEqual(Rational(1, -1), Rational(-1, 1));
+}
+
+void TestPositiveRational() {
+  AssertEqual(Rational(-1, -22), Rational(1, 22));
+  AssertEqual(Rational(-1, -100), Rational(1, 100));
+  AssertEqual(Rational(-3, -100), Rational(3, 100));
+}
+
+void TestZeroNumerator() {
+  AssertEqual(Rational(0, 22), Rational(0, 1));
+  AssertEqual(Rational(0, -100), Rational(0, 1));
 }
 
 int main() {
   TestRunner runner;
-  runner.RunTest(TestZeroAB, "Zero A and B");
-  runner.RunTest(TestZeroANonZeroB, "Zero A and nonzero B");
-  runner.RunTest(TestOthers, "Nonzero A");
+  // 1. Конструктор по умолчанию должен создавать дробь с числителем 0 и
+  // знаменателем 1.
+  runner.RunTest(TestDefaultConstructor, "Default Constructor");
+
+  // 2. При конструировании объекта класса Rational с параметрами p и q должно
+  // выполняться сокращение дроби p/q.
+  runner.RunTest(TestGcd, "Greatest Common Divisor");
+
+  // 3. Если дробь p/q отрицательная, то объект Rational(p, q) должен иметь
+  // отрицательный числитель и положительный знаменатель.
+  runner.RunTest(TestNegativeRational, "Negatives");
+
+  // 4. Если дробь p/q положительная, то объект Rational(p, q) должен иметь
+  // положительные числитель и знаменатель (обратите внимание на случай
+  // Rational(-2, -3)).
+  runner.RunTest(TestPositiveRational, "Positives");
+
+  // 5. Если числитель дроби равен нулю, то знаменатель должен быть равен 1.
+  runner.RunTest(TestZeroNumerator, "Zero numerator");
+
   return 0;
 }
