0c2970ce3c200486ad53afbf3174a5810add6b8b
Rustam <rustam.guseynov@refinitiv.com>
2020-01-03 15:34:57 +0700

    Cleanup

--- test.cpp
+++ test.cpp
@@ -1,21 +1,9 @@
 #include "test.h"
-#include "splitter.h"
 #include "test_runner.h"
 
 void TestThatTestsAreOk() { AssertEqual(1, 1, "1 === 1"); }
-void TestSimpleCase() {
-  {
-    auto res = SplitIntoWords("Hello");
-    AssertEqual(res, vector<string>{"Hello"}, "Hello");
-  }
-  {
-    auto res = SplitIntoWords("Hello World");
-    AssertEqual(res, vector<string>{"Hello", "World"}, "Hello world");
-  }
-}
 
 void RunAllTests() {
   TestRunner runner;
   runner.RunTest(TestThatTestsAreOk, "Tests");
-  runner.RunTest(TestSimpleCase, "Simple case");
 }
