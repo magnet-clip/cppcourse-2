b46b0d8fc1d804f534be60d72c1cc00412fb7549
Rustam <rustam.guseynov@refinitiv.com>
2020-01-17 18:26:05 +0300

    Finally done

--- coursera.cpp
+++ coursera.cpp
@@ -11,16 +11,16 @@ using namespace std;
 class Figure {
 public:
   virtual string Name() const = 0;
-  virtual float Area() const = 0;
-  virtual float Perimeter() const = 0;
+  virtual double Area() const = 0;
+  virtual double Perimeter() const = 0;
 };
 
-class Rectangle : public Figure {
+class Rect : public Figure {
 public:
-  Rectangle(int width, int height) : _width(width), _height(height) {}
+  Rect(int width, int height) : _width(width), _height(height) {}
   virtual string Name() const override;
-  virtual float Area() const override;
-  virtual float Perimeter() const override;
+  virtual double Area() const override;
+  virtual double Perimeter() const override;
 
 private:
   int _width, _height;
@@ -30,8 +30,8 @@ class Circle : public Figure {
 public:
   Circle(int radius) : _radius(radius) {}
   virtual string Name() const override;
-  virtual float Area() const override;
-  virtual float Perimeter() const override;
+  virtual double Area() const override;
+  virtual double Perimeter() const override;
 
 private:
   int _radius;
@@ -41,8 +41,8 @@ class Triangle : public Figure {
 public:
   Triangle(int a, int b, int c) : _a(a), _b(b), _c(c) {}
   virtual string Name() const override;
-  virtual float Area() const override;
-  virtual float Perimeter() const override;
+  virtual double Area() const override;
+  virtual double Perimeter() const override;
 
 private:
   int _a, _b, _c;
@@ -54,7 +54,7 @@ shared_ptr<Figure> CreateFigure(istringstream &ss) {
   if (kind == "RECT") {
     int width, height;
     ss >> width >> height;
-    return make_shared<Rectangle>(width, height);
+    return make_shared<Rect>(width, height);
   } else if (kind == "TRIANGLE") {
     int a, b, c;
     ss >> a >> b >> c;
@@ -68,32 +68,34 @@ shared_ptr<Figure> CreateFigure(istringstream &ss) {
   }
 }
 
-inline string Rectangle::Name() const { return "RECTANGLE"; }
+inline string Rect::Name() const { return "RECT"; }
 
-inline float Rectangle::Area() const { return this->_width * this->_height; }
+inline double Rect::Area() const { return this->_width * this->_height; }
 
-inline float Rectangle::Perimeter() const {
+inline double Rect::Perimeter() const {
   return 2 * (this->_width + this->_height);
 }
 
 inline string Triangle::Name() const { return "TRIANGLE"; }
 
-float Triangle::Area() const {
-  float p = (this->_a + this->_b + this->_c) / 2.0f;
+double Triangle::Area() const {
+  double p = (this->_a + this->_b + this->_c) / 2.0f;
   return sqrt(p * (p - this->_a) * (p - this->_b) * (p - this->_c));
 }
 
-inline float Triangle::Perimeter() const {
+inline double Triangle::Perimeter() const {
   return this->_a + this->_b + this->_c;
 }
 
-const float PI = 3.14;
+const double PI = 3.14;
 
 inline string Circle::Name() const { return "CIRCLE"; }
 
-inline float Circle::Area() const { return PI * this->_radius * this->_radius; }
+inline double Circle::Area() const {
+  return PI * this->_radius * this->_radius;
+}
 
-inline float Circle::Perimeter() const { return 2 * PI * this->_radius; }
+inline double Circle::Perimeter() const { return 2 * PI * this->_radius; }
 
 int main() {
   vector<shared_ptr<Figure>> figures;
