959262b38632c8ad08b10ce325ae7b9b2e86d094
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-01-25 17:03:06 +0300

    Update

--- database.h
+++ database.h
@@ -1,24 +1,22 @@
 #pragma once
 
+#include <functional>
+#include <map>
 #include <sstream>
 #include <string>
 #include <vector>
 #include "date.h"
 
+typedef std::function<bool(Date, std::string)> Predicate;
 class Database {
  public:
-  void Add(const Date &date, const std::string &event) {}
-  void Find(const Date &date) const {}
-  void Print(const std::ostream &ss) const {}
-  bool Last(const Date &date) const {}
-  template <class Predicate>
-  int RemoveIf(Predicate p) const {
-    return 0;
-  }
+  void Add(const Date &date, const std::string &event);
+  void Find(const Date &date) const;
+  void Print(const std::ostream &) const;
+  bool Last(const Date &date) const;
+  int RemoveIf(Predicate predicate);
+  std::vector<std::pair<Date, std::string>> FindIf(Predicate predicate) const;
 
-  // TODO what return type, vector of what?
-  template <class Predicate>
-  std::vector<std::pair<Date, std::string>> FindIf(Predicate p) const {
-    return std::vector<std::pair<Date, std::string>>{};
-  }
+ private:
+  std::map<Date, std::vector<std::string>> data;
 };
